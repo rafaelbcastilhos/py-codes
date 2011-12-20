bcbc4c6de522c57365c7e1bc3349dad4b1aa9461
rafaelbcastilhos <rafaelbcastilhos@gmail.com>
2011-12-20 14:30:00 +0100

    add python concurrence

---
+++ random/charFrequency.py
@@ -0,0 +1,15 @@
+'''This fuction count the char frequency in the given string '''
+def charFrequency(userInput):
+    userInput = userInput.lower() # covert to lowercase
+    dict = {}
+    for char in userInput:
+        keys = dict.keys()
+        if char in keys:
+            dict[char] += 1
+        else:
+            dict[char] = 1
+    return dict
+
+if __name__ == '__main__':
+    userInput = str(input('Enter a string: '))
+    print(charFrequency(userInput))
