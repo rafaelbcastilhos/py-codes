79a416f4c1e87b124d906a8db84e4afc2a35079a
rafaelbcastilhos <rafaelbcastilhos@gmail.com>
2020-12-13 07:00:39 -0300

    final snake game

--- random/snake_game.py
+++ random/snake_game.py
@@ -70,3 +70,58 @@ while not game_over:
         game_over = True
         break
 
+    # check if the snake has collided with the edges
+    if snake[0][0] < 0 or snake[0][1] < 0:
+        game_over = True
+        break
+
+    # ending game
+    if game_over:
+        break
+
+    for i in range(len(snake) - 1, 0, -1):
+        snake[i] = (snake[i - 1][0], snake[i - 1][1])
+
+    # make the snake move
+    if direction == UP:
+        snake[0] = (snake[0][0], snake[0][1] - 10)
+    if direction == DOWN:
+        snake[0] = (snake[0][0], snake[0][1] + 10)
+    if direction == RIGHT:
+        snake[0] = (snake[0][0] + 10, snake[0][1])
+    if direction == LEFT:
+        snake[0] = (snake[0][0] - 10, snake[0][1])
+
+    screen.fill((0, 0, 0))
+    screen.blit(fruit, fruit_position)
+
+    # lines for better viewing
+    for x in range(0, 400, 10):
+        pygame.draw.line(screen, (40, 40, 40), (x, 0), (x, 400))
+    for y in range(0, 400, 10):
+        pygame.draw.line(screen, (40, 40, 40), (0, y), (400, y))
+
+    # show score
+    score_font = font.render('score: %s' % score, True, (15, 159, 255))
+    score_rect = score_font.get_rect()
+    score_rect.topleft = (400 - 60, 0)
+    screen.blit(score_font, score_rect)
+
+    for position in snake:
+        screen.blit(skin_snake, position)
+
+    pygame.display.update()
+
+# game over
+while True:
+    game_over_font = pygame.font.Font('freesansbold.ttf', 78)
+    game_over_screen = game_over_font.render("game over", True, (255, 251, 0))
+    game_over_rect = game_over_screen.get_rect()
+    game_over_rect = (0, 140)
+    screen.blit(game_over_screen, game_over_rect)
+    pygame.display.update()
+
+    while True:
+        for event in pygame.event.get():
+            pygame.quit()
+            exit()
